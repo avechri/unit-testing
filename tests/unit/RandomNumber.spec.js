3e4a7c47365dc70b6510eff7e88193566de90a49
kristina <christychri0@gmail.com>
2021-04-08 17:47:26 +0700

    made a simple tests for RandomNumber.vue component

--- tests/unit/RandomNumber.spec.js
+++ tests/unit/RandomNumber.spec.js
@@ -3,14 +3,34 @@ import { mount } from '@vue/test-utils'
 
 describe('RandomNumber', () => {
   test('By default, randomNumber value should to be 0', () => {
-    expect(true).toBe(false)
+    const wrapper = mount(RandomNumber)
+    expect(wrapper.html()).toContain('<span>0</span>')
   })
 
-  test('If the button is clicked, randomNumber should be between 1 & 10', () => {
-    expect(true).toBe(false)
+  test('If the button is clicked, randomNumber should be between 1 & 10', async () => {
+    const wrapper = mount(RandomNumber)
+
+    wrapper.find('button').trigger('click')
+
+    await wrapper.vm.$nextTick()
+    const randomNumber = parseInt(wrapper.find('span').element.textContent)
+    expect(randomNumber).toBeGreaterThanOrEqual(1)
+    expect(randomNumber).toBeLessThanOrEqual(10)
   })
 
-  test('If the button is clicked, randomNumber should be between 200 & 300', () => {
-    expect(true).toBe(false)
+  test('If the button is clicked, randomNumber should be between 200 & 300', async () => {
+    const wrapper = mount(RandomNumber, {
+      propsData: {
+        min: 200,
+        max: 300,
+      },
+    })
+
+    wrapper.find('button').trigger('click')
+
+    await wrapper.vm.$nextTick()
+    const randomNumber = parseInt(wrapper.find('span').element.textContent)
+    expect(randomNumber).toBeGreaterThanOrEqual(200)
+    expect(randomNumber).toBeLessThanOrEqual(300)
   })
 })
