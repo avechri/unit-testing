f03af40e094c01ce583aea7d5887cc15cd593ebe
kristina <christychri0@gmail.com>
2021-04-08 17:10:00 +0700

    created a simple RundomNumber component

---
+++ src/components/RandomNumber.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <span>{{ randomNumber }}}</span>
+    <button @click="getRundomNumber">Generate random number</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    min: {
+      type: Number,
+      default: 1,
+    },
+    max: {
+      type: Number,
+      default: 10,
+    },
+  },
+  data() {
+    return {
+      randomNumber: 0,
+    }
+  },
+  methods: {
+    getRundomNumber() {
+      this.randomNumber = Math.floor(
+        Math.random() * (this.max - this.min + 1) + this.min
+      )
+    },
+  },
+}
+</script>
+
+<style scoped></style>
